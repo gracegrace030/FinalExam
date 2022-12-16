77f99951f7a09720f2ea7ee24da83793396f5e27
gracegrace030 <113151468+gracegrace030@users.noreply.github.com>
2022-12-15 22:17:02 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ myImmutableArray.shift();
 myImmutableArray.slice(1,2);
 
 // Step 6: Using array filter, iterate over the array items and remove all items beginning with the letter "J". Store the result in a new, block scoped, mutable variable
-
+var nameWithoutJ = myImmutableArray.filter((name) => {return name[0] !== 'J'});
 
 // Step 7: Create an event listener that fires a callback function when the call to action button in the header is clicked. It should alert "Call me now at 555-555-55555";
 const actionBtn = document.querySelector("button");
